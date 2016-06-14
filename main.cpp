d50dd0985d0561ed5f8d6063ca76cc47f9f1ab7c
defelice <ad.defelice@live.com>
2016-06-14 19:11:14 -0400

    refactoring, start of calculating for z

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+/**
+ * a simple renderer
+ * - adam defelice
+ *
+ * this needs to be refactored :/
+ *
+ */
+
 #include "tgaimage.h"
 #include "model.h"
 #include <cmath>
 #include <iostream>
+#include <vector>
+#include <map>
 
 const TGAColor WHITE = TGAColor(255, 255, 255, 255);
 const TGAColor BLUE  = TGAColor(  0,   0, 255, 255);
@@ -16,25 +26,31 @@ enum DRAW_MODE {WIRE, FILL};
 
 // returns a vector of points on the heap
 // this is a memory leak if you don't deallocate the returned vector :X
-std::vector<Vec3i*> line(Vec3i &v0, Vec3i &v1, TGAImage &image, TGAColor color) {
+// a fact: this is the only function that calls image.set()
+std::vector<Vec3i*> line(Vec3i *v0, Vec3i *v1, TGAImage &image, TGAColor color, std::map<Vec2i, int> &z_values, bool draw) {
 
-  int x0 = v0.x;
-  int y0 = v0.y;
-  int z0 = v0.z;
-  int x1 = v1.x;
-  int y1 = v1.y;
-  int z1 = v1.z;
-  // time to rewrite swapping + transposing so that we're swapping pointers to Vec3s
+  // TODO: make this calculate Z, so that we can ignore them later
 
+  int x0 = v0->x;
+  int y0 = v0->y;
+  int z0 = v0->z;
+  int x1 = v1->x;
+  int y1 = v1->y;
+  int z1 = v1->z;
+
+  // this vector will hold the points that make up the line. we'll be returning it.
   std::vector<Vec3i*> points;
-  bool isTransposed = false;
+
+  /* DETERMINE Y VALUES FOR EACH X */
+  // heck, looks like i'm going to dubplicate this all at the bottom for (x, z)
+  bool yTransposed = false;
   // if the rise is greater than the run, transpose
   // we do this before potentially swapping points so that we don't end up un-swapping them
   if (std::abs(y1 - y0) > std::abs(x1 - x0)) {
     std::swap(x0, y0);
     std::swap(x1, y1);
-    isTransposed = true;
-    printf("transposed!\n");
+    yTransposed = true;
+    printf("x + y transposed!\n");
   }
 
   // ensure that (x0, y0) is the leftmost point (closest to zero on the positive side)
@@ -55,20 +71,36 @@ std::vector<Vec3i*> line(Vec3i &v0, Vec3i &v1, TGAImage &image, TGAColor color) 
   printf("%d -> %d\n", x0, x1);
   // draw the points, first point is (x0, y0)
   for (int x = x0; x <= x1; x++) {
-    if (isTransposed) {
-      // push the point to our point array
-      points.push_back(new Vec3i(y, x, 0));
-      // draw the point
-      image.set(y, x, color);
-      // print coords
-      printf("%d, %d\n", y, x);
+
+    // handle transposition
+    int x_to_draw;
+    int y_to_draw;
+    if (yTransposed) {
+      x_to_draw = y;
+      y_to_draw = x;
     } else {
-      points.push_back(new Vec3i(x, y, 0));
-      image.set(x, y, color);
-      printf("%d, %d\n", x, y);
+      x_to_draw = x;
+      y_to_draw = y;
     }
-    // add overflow
-    // a problem? the offset can only ever deliver 1 y of error per x
+
+/*
+    // break early if we've already drawn a point with a higher z value
+    Vec2i z_key(x_to_draw, y_to_draw);
+    if (z_values.count(z_key) != 0) {
+      if (z_values.at(z_key) < z){
+        // code
+      }
+    } else {
+      // found
+    }
+*/
+    // record the point, maybe draw
+    Vec3i *point = new Vec3i(x_to_draw, y_to_draw, 0);
+    points.push_back(point);
+    if (draw) image.set(x_to_draw, y_to_draw, color);
+
+    // add overflow for the next point
+    // TODO? the offset can only ever deliver 1 y of error per x
     error += slope;
     if (error >= 1.0) {
       error--;
@@ -78,12 +110,14 @@ std::vector<Vec3i*> line(Vec3i &v0, Vec3i &v1, TGAImage &image, TGAColor color) 
       y--;
     }
   }
+
   printf("\n");
   return points;
 }
 
 // i have a feeling that this will skip the bottom line
-void fill_triangle(std::vector<Vec3i*> &points, TGAImage &image) {
+// points = triangle edge points
+void draw_triangle(std::vector<Vec3i*> &points, TGAImage &image, std::map<Vec2i, int> &z_values) {
 
   std::vector<Vec3i*> horizontal_points;
   int previous_y;
@@ -118,7 +152,8 @@ void fill_triangle(std::vector<Vec3i*> &points, TGAImage &image) {
         horizontal_points.back()->z
       );
       // std::cout << "we're drawing: (" << x0 << ", " << y0 <<"), (" << x1 << ", " << y1 << ")" << std::endl;
-      line(v0, v1, image, TEAL);
+      // draw + fill
+      line(&v0, &v1, image, TEAL, z_values, true);
       horizontal_points.clear();
       horizontal_points.push_back(*i);
       previous_y = (*i)->y;
@@ -132,22 +167,23 @@ void draw_object(Model &m, TGAImage &image, TGAColor color, bool z_culling, DRAW
 
   // let's cull some z
   // *shotgun pump sound*
-  std::vector<Vec3i> points_seen;
+  std::map<Vec2i, int> z_values;
 
-  // for each face
+  // for each of the object's faces
   for (int i = 0; i < m.nfaces(); ++i) {
 
     // a face is a vector of ints, copy it
     std::vector<int> face = m.face(i);
-    //if (mode == FILL) {
     // we'll store three lines of points at a time
     // this is going to allocate some pointers to memory, so we'll have to clear it later
     std::vector<Vec3i*> points_face;
-    // for each line
+
+    // for each line of the face's three lines
     for (int j = 0; j < 3; ++j) {
+
+      /* NORMALIZE POINTS */
       Vec3f v0 = m.vert(face[j]);
       Vec3f v1 = m.vert(face[(j+1)%3]);
-
       // add 1 to each point to make all numbers positive, then scale by dimension
       Vec3i v0_i(
         (v0.x+1.0)*WIDTH/2,
@@ -159,12 +195,17 @@ void draw_object(Model &m, TGAImage &image, TGAColor color, bool z_culling, DRAW
         (v1.y+1.0)*HEIGHT/2,
         (v1.z+1.0)*DEPTH/2
       );
+      printf("Normalized outline vertices: (%d, %d, %d), (%d, %d, %d)\n", v0_i.x, v0_i.y, v0_i.z, v1_i.x, v1_i.y, v1_i.z);
 
-      printf("(%d, %d, %d), (%d, %d, %d)\n", v0_i.x, v0_i.y, v0_i.z, v1_i.x, v1_i.y, v1_i.z);
-      // get and (unnecessarily) draw the triangle's outer lines
-      // this should merge the return value of lines() with points' current value
-      std::vector<Vec3i*> points_line = line(v0_i, v1_i, image, color);
-      points_face.insert(points_face.begin(), points_line.begin(), points_line.end());
+      // if we're not filling the triangles, we will just draw their outlines and leave
+      if (mode == WIRE) {
+        line(&v0_i, &v1_i, image, color, z_values, true);
+      } else {
+        // get the points of the triangle's outer lines, but don't waste time drawing them
+        std::vector<Vec3i*> points_line = line(&v0_i, &v1_i, image, color, z_values, false);
+        // merge the face's outline vertices with our running collection of face outline vertices
+        points_face.insert(points_face.begin(), points_line.begin(), points_line.end());
+      }
     }
 
     // we have all of the outer points of the triangle
@@ -174,7 +215,7 @@ void draw_object(Model &m, TGAImage &image, TGAColor color, bool z_culling, DRAW
       points_face.end(),
       [] (Vec3i *a, Vec3i *b) { return a->y < b->y; }
     );
-    fill_triangle(points_face, image);
+    draw_triangle(points_face, image, z_values);
 
     // deallocate memory
     for (std::vector<Vec3i*>::iterator it = points_face.begin() ; it != points_face.end(); ++it) {
@@ -185,12 +226,19 @@ void draw_object(Model &m, TGAImage &image, TGAColor color, bool z_culling, DRAW
   } // foreach face
 }
 
+// [executable] wire
+//   - draws wireframe instead of fill
 int main(int argc, char* argv[]) {
+
+  // parse args
+  DRAW_MODE mode = FILL;
+  if (argc > 1) {
+    if (strncmp(argv[1], "wire", 4) == 0) mode = WIRE;
+  }
+
   TGAImage image(WIDTH, HEIGHT, TGAImage::RGB);
   Model model("./african_head.obj");
-
-  // next step ... filling triangles or z culling
-  draw_object(model, image, GREEN, false, FILL);
+  draw_object(model, image, RED, false, mode);
   image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
   image.write_tga_file("output.tga");
   return 0;
