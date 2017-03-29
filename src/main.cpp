57b3ef7762c01ab349e3f677d9d4a83b26b27e9a
adefelic <ad.defelice@live.com>
2017-03-29 14:47:45 -0400

    fixed bug where zbuffer size could blow up stack

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,25 @@
 #include <array>
 #include <map>
 
-const auto WIDTH  = 800;
-const auto HEIGHT = 800;
-const auto AREA = WIDTH*HEIGHT;
+const auto WIDTH = 2048;
+const auto HEIGHT = 2048;
+const auto AREA = WIDTH * HEIGHT;
 
 // convert from world coordinates to screen coordinates
 // add 1 to each point to make all numbers positive, then scale by dimension
-Vec3f convert_to_screen_coords(Vec3f point) {
+Vec3f convert_to_screen_coordinates(Vec3f point) {
 	auto c = 4; // camera's distance from the origin in the positive z direction
 
 	// project onto the plane z=1
-	auto x = point.x / (1 - point.z/c);
-	auto y = point.y / (1 - point.z/c);
-	auto z = point.z / (1 - point.z/c);
+	auto x = point.x / (1 - point.z / c);
+	auto y = point.y / (1 - point.z / c);
+	auto z = point.z / (1 - point.z / c);
 
 	auto scale = std::sqrt(AREA);
 	return Vec3f(
-		(x+1.0)*scale/2,
-		(y+1.0)*scale/2,
-		(z+1.0)*scale/2
+		float((x + 1.0) * scale / 2),
+		float((y + 1.0) * scale / 2),
+		float((z + 1.0) * scale / 2)
 	);
 }
 
@@ -42,32 +42,27 @@ TGAColor get_illumination(Vec3f &normal, Vec3f &light_source) {
 	auto l = light_source.normalize();
 
 	// now find the angle between the directional light and the normal (theta from here forwards)
-	auto cos_theta = dot_product(n, l) / vector_magnitude(n, Vec3f(0,0,0)) * vector_magnitude(l, Vec3f(0,0,0));
+	auto cos_theta = dot_product(n, l) / vector_magnitude(n, Vec3f(0, 0, 0)) * vector_magnitude(l, Vec3f(0, 0, 0));
 
 	// cos_theta will be between -1.0 and 1.0
-	auto brightness = round(255 * (pow((1+cos_theta)/2, 0.95)));
+	auto brightness = lround(255 * ((1 + cos_theta) / 2));
 	return TGAColor(brightness, brightness, brightness, 255);
 }
 
-/*
-	TGAColor get_pixel_illumination(Vec3f vertex_normal){
-		return TGAColor(0,0,0,0);
-	}
-*/
 // rasterize the triangle described by vertices a b c onto the passed TGAImage
-void draw_face(Face &face, TGAImage &image, std::array<double, AREA> &zbuffer, TGAImage &texture, Vec3f &light_source) {
+void draw_face(Face &face, TGAImage &image, std::unique_ptr<std::array<double, AREA>> &zbuffer, TGAImage &texture, Vec3f &light_source) {
 
-	auto a = convert_to_screen_coords(face.get_vertices()[0].get_position());
-	auto b = convert_to_screen_coords(face.get_vertices()[1].get_position());
-	auto c = convert_to_screen_coords(face.get_vertices()[2].get_position());
+	auto a = convert_to_screen_coordinates(face.get_vertices()[0].get_position());
+	auto b = convert_to_screen_coordinates(face.get_vertices()[1].get_position());
+	auto c = convert_to_screen_coordinates(face.get_vertices()[2].get_position());
 
 	auto at = face.get_vertices()[0].get_texture_coordinates();
 	auto bt = face.get_vertices()[1].get_texture_coordinates();
 	auto ct = face.get_vertices()[2].get_texture_coordinates();
 
 	auto an = face.get_vertices()[0].get_normal();
-	auto bn = face.get_vertices()[0].get_normal();
-	auto cn = face.get_vertices()[0].get_normal();
+	auto bn = face.get_vertices()[1].get_normal();
+	auto cn = face.get_vertices()[2].get_normal();
 
 	// of the triangle's corner vertices, find the maxes and mins of x and y.
 	// those describe the bounding box
@@ -75,19 +70,18 @@ void draw_face(Face &face, TGAImage &image, std::array<double, AREA> &zbuffer, T
 	std::vector<float> y_extrema = {a.y, b.y, c.y};
 
 	// this is where we go from floating point to integral
-	int x_max = std::round(*max_element(x_extrema.begin(), x_extrema.end()));
-	int x_min = std::round(*min_element(x_extrema.begin(), x_extrema.end()));
-	int y_max = std::round(*max_element(y_extrema.begin(), y_extrema.end()));
-	int y_min = std::round(*min_element(y_extrema.begin(), y_extrema.end()));
+	auto x_max = static_cast<int>(std::round(*max_element(x_extrema.begin(), x_extrema.end())));
+	auto x_min = static_cast<int>(std::round(*min_element(x_extrema.begin(), x_extrema.end())));
+	auto y_max = static_cast<int>(std::round(*max_element(y_extrema.begin(), y_extrema.end())));
+	auto y_min = static_cast<int>(std::round(*min_element(y_extrema.begin(), y_extrema.end())));
 
 	// iterate over each point in the bounding box
-	for (int x = x_min; x < x_max + 1; ++x) {
+	for (auto x = x_min; x < x_max + 1; ++x) {
 		// bounds check
 		if (x < 0 || x >= WIDTH) continue;
-		for (int y = y_min; y < y_max + 1; ++y) {
+		for (auto y = y_min; y < y_max + 1; ++y) {
 			// bounds check
-			if (y < 0 || y >= HEIGHT ) continue;
-
+			if (y < 0 || y >= HEIGHT) continue;
 			Vec2i point(x, y);
 			// find the barycentric weight point P within the triangle
 			Vec3f barycentric_weights = barycentric(point, a, b, c);
@@ -115,9 +109,9 @@ void draw_face(Face &face, TGAImage &image, std::array<double, AREA> &zbuffer, T
 				y_t += ct.y * barycentric_weights.z;
 
 				// check in with our z buffer
-				if (zbuffer[x + y * WIDTH] < z) {
+				if ((*zbuffer)[x + y * WIDTH] < z) {
 					// add our new highest z value
-					zbuffer[x + y * WIDTH] = z;
+					(*zbuffer)[x + y * WIDTH] = z;
 					// map a color from the texture to the pixel we're drawing
 					TGAColor tex_color = texture.get(
 						x_t * (texture.get_width()),
@@ -132,11 +126,11 @@ void draw_face(Face &face, TGAImage &image, std::array<double, AREA> &zbuffer, T
 
 					auto fragment_illumination = get_illumination(fragment_normal, light_source);
 
-					// interpolate texel color w/ face color via lighting
+					// interpolate texel color w/ fragment color from light
 					TGAColor pixel_color(
-						(int)tex_color.r * (int)fragment_illumination.r / 255.0,
-						(int)tex_color.g * (int)fragment_illumination.g / 255.0,
-						(int)tex_color.b * (int)fragment_illumination.b / 255.0,
+						int(tex_color.r) * int(fragment_illumination.r) / 255,
+						int(tex_color.g) * int(fragment_illumination.g) / 255,
+						int(tex_color.b) * int(fragment_illumination.b) / 255,
 						255
 					);
 					// draw
@@ -148,11 +142,7 @@ void draw_face(Face &face, TGAImage &image, std::array<double, AREA> &zbuffer, T
 }
 
 // draw a model to an image
-void draw_model(Model &m, TGAImage &texture, TGAImage &image, Vec3f &light_source) {
-
-	// init z buffer
-  std::array<double, AREA> zbuffer{};
-  zbuffer.fill(0);
+void draw_model(Model &m, TGAImage &texture, TGAImage &image, std::unique_ptr<std::array<double, AREA>> &zbuffer, Vec3f &light_source) {
 
 	// for each face
 	// TODO: Models, as declared in model.h, do not have an iterable face collection :(
@@ -162,12 +152,12 @@ void draw_model(Model &m, TGAImage &texture, TGAImage &image, Vec3f &light_sourc
 }
 
 // render an image
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
 	// create a light source
 	// points from here towards origin
 	// ignores occlusion
-	auto light_source = Vec3f(2.0, 1.0, 1.0);
+	auto light_source = Vec3f(1.0, 0.0, 1.0);
 
 	// load model
 	// TODO: this boilerplate is not ideal, i should rewrite it
@@ -178,7 +168,7 @@ int main(int argc, char* argv[]) {
 	texture.read_tga_file("../data/african_head_diffuse.tga");
 	texture.flip_vertically();
 
-	// initialize output image
+	// init output image
 	auto image = TGAImage(WIDTH, HEIGHT, TGAImage::RGB);
 	// fill the image with a background color because the glare on my screen is fierce
 	for (auto i = 0; i < WIDTH; ++i) {
@@ -186,12 +176,15 @@ int main(int argc, char* argv[]) {
 			image.set(i, j, TGAColor(200, 200, 200, 255));
 		}
 	}
+	// init z buffer
+	auto zbuffer = std::make_unique<std::array<double, AREA>>();
+	zbuffer->fill(0);
 
 	// draw model to image
-	draw_model(model, texture, image, light_source);
+	draw_model(model, texture, image, zbuffer, light_source);
 
 	// write image to file
 	image.flip_vertically();
-	image.write_tga_file("output.tga");
+	image.write_tga_file("../data/output.tga");
 	return 0;
 }
